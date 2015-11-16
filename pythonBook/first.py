8e2cee322ab7247270b800f577c13b8de5c9892e
David Hernandez <David@dcypherops.org>
2015-11-15 23:10:52 -0700

    few py files

---
+++ pythonBook/first.py
@@ -0,0 +1,16 @@
+#Author: David Hernandez
+#Specification: Print words
+
+# this is a python code
+print ("Hello World")
+print "no need for these () in the code"
+print "just printing more things"
+print "now I'm just bored"
+print "I should really be doing my homework instead"
+print "nahhhh"
+'''
+this comments out
+multiple lines
+in python
+code
+'''
---
+++ pythonBook/math.py
@@ -0,0 +1,22 @@
+#Author: David Hernandez
+#Specification: Prints and does math
+
+#addition
+print "what is 2 + 2"
+print 2 + 2
+
+#subtraction
+print "What is 5 - 7 ", 5 - 7
+
+#this prints the remainder of a divison
+print 40 % 6
+
+#this prints 2 times 5
+print "2 * 5 ", 2 * 5
+
+#this prints 30 devided by 5
+print "30 / 5 ", 30 / 5
+
+print 7.0 / 4.0
+
+print 7 / 4
