b48e999625b8ba73b9e84edd4f6fdd960c152968
David Hernandez <David@dcypherops.org>
2015-11-16 12:30:50 -0700

    comments added

--- pythonBook/assign_variables.py
+++ pythonBook/assign_variables.py
@@ -2,5 +2,7 @@
 #Description: assigning variables to other variables
 car = 100
 train = car
+# in other words a single = sign will copy the value from the left to the variable to the right
+
 
 print train
