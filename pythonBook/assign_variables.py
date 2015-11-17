3695148ac37fdcb698a3cf585005ad3fa6137837
David Hernandez <David@dcypherops.org>
2015-11-16 23:18:18 -0700

    removed

--- pythonBook/assign_variables.py
+++
@@ -1,8 +0,0 @@
-#Author: David Hernandez
-#Description: assigning variables to other variables
-car = 100
-train = car
-# in other words a single = sign will copy the value from the left to the variable to the right
-
-
-print train
