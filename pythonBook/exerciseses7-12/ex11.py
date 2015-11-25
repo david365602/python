5c18a1e1b43f0d676b66fd8f8028e776e14e6cc7
David Hernandez <David@dcypherops.org>
2015-11-25 16:15:41 -0700

    ex9B needs more testing

---
+++ pythonBook/exerciseses7-12/ex11.py
@@ -0,0 +1,14 @@
+#ex11.py
+#asking questions 
+
+print "How are old you?",
+age = raw_input()
+
+print "How tall are you?",
+height = raw_input()
+
+print "How much do weight?",
+weight = raw_input()
+
+print "So you are %r old, %r tall, and %r heavy" % (
+	age, height, weight)
---
+++ pythonBook/exerciseses7-12/ex9B.py
@@ -0,0 +1,6 @@
+#ex9B.py
+# Mystery code
+while True:
+	for i in ["/","-","|","\\","|"]:
+		print "%s\r" % i,
+#pretty cool code
