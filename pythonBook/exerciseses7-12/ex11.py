7eb7e30e629fb864bdbf8ce2b471699784c538fd
David Hernandez <David@dcypherops.org>
2015-12-24 01:11:57 -0700

    more practice and learning

--- pythonBook/exerciseses7-12/ex11.py
+++ pythonBook/exerciseses7-12/ex11.py
@@ -10,5 +10,11 @@ height = raw_input()
 print "How much do weight?",
 weight = raw_input()
 
+print "numbers to multiply? enter 2: "
+x = int(raw_input())
+y = int(raw_input())
+
+
 print "So you are %r old, %r tall, and %r heavy" % (
 	age, height, weight)
+print ("%d multiplied by %d is equal to %d") % (x, y, x*y)
---
+++ pythonBook/exerciseses7-12/ex12.py
@@ -0,0 +1,11 @@
+#ex12
+#prompting people
+
+age = raw_input("How are old you? ")
+
+height = raw_input("How tall are you? ")
+
+weight = raw_input("How much do weight? ")
+
+print "So you are %s old, %s tall, and %s heavy" % (
+	age, height, weight)
