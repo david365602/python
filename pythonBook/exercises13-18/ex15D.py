61e3b4abdd4afcc2aa4b6fbbaf59527d045bf54e
David Hernandez <David@dcypherops.org>
2015-12-27 02:50:53 -0700

    late night work

---
+++ pythonBook/exercises13-18/ex15D.py
@@ -0,0 +1,18 @@
+#ex15D.py
+#write words to a file
+#not working yet
+
+print "enter the name of the file you want to write to:",
+txt = raw_input()
+
+#this line gives the file a variable
+#the w is important as it tells python that it will be used 
+#for writing
+filename = open(txt, 'w')
+filename.truncate()
+
+print "enter the words you want to add to the file"
+words = raw_input()
+
+filename.write(words)
+filename.close()
