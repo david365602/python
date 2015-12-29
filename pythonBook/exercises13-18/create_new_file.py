0763458ae96d709f9b9c26ba458c06fb6c1ec09b
David Hernandez <David@dcypherops.org>
2015-12-28 23:15:04 -0700

    finally got this to work!

--- pythonBook/exercises13-18/create_new_file.py
+++ pythonBook/exercises13-18/create_new_file.py
@@ -5,7 +5,7 @@ print "enter the name of the new file"
 name = raw_input()
 
 #opens the file and makes it read and writable
-filename = open(name, 'w+')
+filename = open(name, 'a+')
 
 #asks for text that is to be added
 print "enter the text you want to give your file"
@@ -14,12 +14,14 @@ txt = raw_input()
 #text is added to file
 filename.write(txt)
 
-#this part does not work just yet
-'''
+#this part does not work just yet
+
 print "All done now. \nYour file reads"
 #broken after this point
-print filename.read()
-'''
+
+filename.seek(0)
+print (filename.read())
+
 
 #closes file
 filename.close()
